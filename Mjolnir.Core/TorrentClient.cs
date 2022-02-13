36017a1e78862ffce60b6955acdba46b6dae765e
roxxel <warperthe@gmail.com>
2022-02-13 19:35:43 +0200

    Add project files

---
+++ Mjolnir.Core/TorrentClient.cs
@@ -0,0 +1,23 @@
+﻿using SuRGeoNix.BitSwarmLib;
+
+namespace Mjolnir.Core
+{
+    public class TorrentClient
+    {
+        private BitSwarm _bitSwarm;
+
+        public TorrentClient(string torrent)
+        {
+            _bitSwarm = new BitSwarm(null);
+            _bitSwarm.Open(torrent);
+        }
+
+        public async Task StartDownloading()
+        {
+            _ = Task.Run(() =>
+            {
+                _bitSwarm.Start();
+            });
+        }
+    }
+}
